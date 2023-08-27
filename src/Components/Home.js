ce32071fc2cb022fb0fdea98d1effae217a12f89
saeedyussif663 <saeedyussif663@gmail.com>
2023-08-27 10:52:30 +0100

    restyled the home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,9 +4,9 @@
 const Home = () => {
 
     return (
-        <div className="home-container">
-            <h1 className="text-4xl text-white mb-12">welocme to pixabay</h1>
-            <h1 className="text-4xl text-white mb-12">search for your favourie photos</h1>
+        <div className="home-container w-11/12 md:mx-auto absolute bottom-6">
+            <h1 className="text-xl md:text-4xl text-white mb-4 text-center">welcome to pixabay</h1>
+            <h1 className="text-xl md:text-4xl text-white mb-4 text-center">search for your favourie photos</h1>
             <i className="fa-regular fa-bin"></i>
         </div>
     )
